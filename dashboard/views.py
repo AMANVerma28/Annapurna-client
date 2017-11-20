1853bb5bf1e53273fdcd869afabcd36e4fff6a02
AMANVerma28 <amansingh.v15@iiits.in>
2017-11-20 22:34:20 +0530

    Changes in charts

--- dashboard/views.py
+++ dashboard/views.py
@@ -11,31 +11,6 @@ from django.http import Http404,HttpRequest
 #When the data is recieved, it is stored in the static folder as a json file.
 #Home paga requests data of each model from server.
 def index(request):
-    data = coreapi.Client()
-    schema = data.get("https://itsproject-server.herokuapp.com/household/")
-    with open('static/json/household.json', 'w') as outfile:
-        json.dump(schema, outfile)
-    schema = data.get("https://itsproject-server.herokuapp.com/farm/")
-    with open('static/json/farm.json', 'w') as outfile:
-	    json.dump(schema, outfile)
-    schema = data.get("https://itsproject-server.herokuapp.com/well/")
-    with open('static/json/well.json', 'w') as outfile:
-        json.dump(schema, outfile)
-    schema = data.get("https://itsproject-server.herokuapp.com/member/")
-    with open('static/json/member.json', 'w') as outfile:
-        json.dump(schema, outfile)
-    schema = data.get("https://itsproject-server.herokuapp.com/season/")
-    with open('static/json/season.json', 'w') as outfile:
-        json.dump(schema, outfile)
-    schema = data.get("https://itsproject-server.herokuapp.com/crop/")
-    with open('static/json/crop.json', 'w') as outfile:
-        json.dump(schema, outfile)
-    schema = data.get("https://itsproject-server.herokuapp.com/storage/")
-    with open('static/json/storage.json', 'w') as outfile:
-        json.dump(schema, outfile)
-    schema = data.get("https://itsproject-server.herokuapp.com/yield/")
-    with open('static/json/yield.json', 'w') as outfile:
-        json.dump(schema, outfile)
     return render(request,'dashboard/index.html')   
 
 #View for charts.
